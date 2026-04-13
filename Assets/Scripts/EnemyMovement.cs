6e5ff3db8e9b1a82327a4e28bf181e763823e053
looeychewy <143098413+looeychewy@users.noreply.github.com>
2026-04-12 20:44:43 -0400

    4/12 8:44 reimplementation
    Core mechanic reimplemntation (collisions, enemy startup, etc.)

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed = 7f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed;
     }
 }
