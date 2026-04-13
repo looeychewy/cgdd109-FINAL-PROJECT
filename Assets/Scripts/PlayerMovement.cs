6c708cbb588f8996c7ba0220241ff0db7a52c500
looeychewy <143098413+looeychewy@users.noreply.github.com>
2026-04-12 20:23:31 -0400

    redo 4/12 8:23 pm
    collision bugs -> start from scratch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float maxSpeed = 5f;
-
-    private Rigidbody2D rb;
-
-    float moveX;
-    float moveY;
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxisRaw("Horizontal");
-        moveY = Input.GetAxisRaw("Vertical");
-    }
-
-    void FixedUpdate()
-    {
-        Vector2 dir = new Vector2(moveX, moveY);
-        rb.velocity = dir * maxSpeed;
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Target"))
-        {
-            score += 1;
-            other.gameObject.SetActive(false);
-            Debug.Log("Picked up! Score = " + score);
-        }
+        
     }
 }
